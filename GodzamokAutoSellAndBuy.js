8c777ddacd68d548015115225fcaaee867da693b
Vince Lugli <vilugli@microsoft.com>
2019-01-31 18:29:22 -0800

    Initial commit: adding functionality to sell then rebuy all items via hotkey. Update to README.

---
+++ GodzamokAutoSellAndBuy.js
@@ -0,0 +1,62 @@
+GodzamokAuto = {}
+
+GodzamokAuto.itemsSold = [];
+
+GodzamokAuto.sellItems = function (maxSale, shouldSkipGrandma = true) {
+    document.getElementById('storeBulkSell').click();
+    document.getElementById('storeBulkMax').click();
+    
+    for (let i = 0; i < maxSale; i++) {
+        if (i === 1 && shouldSkipGrandma) {
+            continue;
+        }
+
+        const numberOfItems = document.getElementById(`productOwned${i}`).innerHTML;
+        itemsSold[i] = numberOfItems;
+
+        // click item to sell all
+        document.getElementById(`product${i}`).click();
+    }
+}
+
+GodzamokAuto.buyBackItems = function (maxSale) {
+    document.getElementById('storeBulkBuy').click();
+    const buy100 = document.getElementById('storeBulk100');
+    const buy10 = document.getElementById('storeBulk10');
+    const buy1 = document.getElementById('storeBulk1');
+
+    for (let i = 0; i < itemsSold.length; i++) {
+        const itemElement = document.getElementById(`product${i}`);
+
+        buy100.click();
+        GodzamokAuto.buyUntilN(itemElement, 100, i);
+        buy10.click();
+        GodzamokAuto.buyUntilN(itemElement, 10, i);
+        buy1.click();
+        GodzamokAuto.buyUntilN(itemElement, 1, i);
+    }
+}
+
+GodzamokAuto.buyUntilN = function (element, n, i) {
+    while (itemsSold[i] >= n) {
+        element.click();
+        itemsSold[i] -= n;
+    }
+}
+
+document.onkeyup = function(e) {
+    if (e.ctrlKey) {
+        const numberPressed = e.which - 49;
+
+        if (numberPressed > 10) {
+            return;
+        }
+
+        if (e.shiftKey) {
+            GodzamokAuto.buyBackItems(numberPressed);
+        }
+        else {
+            GodzamokAuto.sellItems(numberPressed);
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,26 @@
 # GodzamokMod
-Auto sell and re-buy items with single keypress
+Auto sell and re-buy items with single keypress.
+
+## Installing
+
+Create a bookmark and add the following as the URL.
+
+```
+javascript: (function () {
+	Game.LoadMod('https://raw.githubusercontent.com/vincelugli/GodzamokMod/master/GodzamokAutoSellAndBuy.js');
+}());
+```
+
+Alternatively, you can run the follow function from dev tools console.
+
+```
+(function () {
+	Game.LoadMod('https://raw.githubusercontent.com/vincelugli/GodzamokMod/master/GodzamokAutoSellAndBuy.js');
+}());
+```
+
+## How to use
+
+Once the mod is loaded, press Ctrl + a number to sell all builds (except grandmas) at and below that level. For example, `Ctrl + 5` will sell all Cursors, Farms, Mines, and Factories. `Ctrl + 3` will sell only Cursors, and Farms.
+
+To re-buy everything, press Ctrl + Shift + the same number. In the same examples above, you would press `Ctrl + Shift + 5` and `Ctrl + Shift + 3`.
